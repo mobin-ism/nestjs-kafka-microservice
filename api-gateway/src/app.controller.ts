d7d12cd8dee910891dc5d58d41e3689b9baadc77
Al Mobin <instance.of.venture@gmail.com>
2023-04-27 13:28:27 +0600

    Vscode settings and prettier file has been added

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,20 +1,20 @@
-import { Controller, Get, Inject, OnModuleInit } from '@nestjs/common';
-import { AppService } from './app.service';
-import { ClientKafka, MessagePattern } from '@nestjs/microservices';
+import { Controller, Get, Inject, OnModuleInit } from '@nestjs/common'
+import { ClientKafka } from '@nestjs/microservices'
+import { AppService } from './app.service'
 
 @Controller('api')
 export class AppController implements OnModuleInit {
-  constructor(
-    @Inject('GENRE-SERVICE')
-    private readonly genreClient: ClientKafka,
-    private readonly appService: AppService,
-  ) {}
-  onModuleInit() {
-    this.genreClient.subscribeToResponseOf('get-genre');
-  }
+    constructor(
+        @Inject('GENRE-SERVICE')
+        private readonly genreClient: ClientKafka,
+        private readonly appService: AppService
+    ) {}
+    onModuleInit() {
+        this.genreClient.subscribeToResponseOf('get-genre')
+    }
 
-  @Get('genre')
-  getGenre() {
-    return this.appService.getGenre('Al Mobin');
-  }
+    @Get('genre')
+    getGenre() {
+        return this.appService.getGenre('Al Mobin')
+    }
 }
--- api-gateway/src/app.service.ts
+++ api-gateway/src/app.service.ts
@@ -1,28 +1,27 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { ClientKafka } from '@nestjs/microservices';
-import { firstValueFrom } from 'rxjs';
+import { Inject, Injectable } from '@nestjs/common'
+import { ClientKafka } from '@nestjs/microservices'
 
 @Injectable()
 export class AppService {
-  constructor(
-    @Inject('GENRE-SERVICE') private readonly genreClient: ClientKafka,
-  ) {}
+    constructor(
+        @Inject('GENRE-SERVICE') private readonly genreClient: ClientKafka
+    ) {}
 
-  async getGenre(user: string) {
-    return new Promise((resolve, reject) => {
-      this.genreClient.send('get-genre', user).subscribe({
-        next: (data) => {
-          console.log('received data:', data);
-          resolve(data);
-        },
-        error: (error) => {
-          console.error('error occurred:', error);
-          reject(error);
-        },
-        complete: () => {
-          console.log('completed');
-        },
-      });
-    });
-  }
+    async getGenre(user: string) {
+        return new Promise((resolve, reject) => {
+            this.genreClient.send('get-genre', user).subscribe({
+                next: (data) => {
+                    console.log('received data:', data)
+                    resolve(data)
+                },
+                error: (error) => {
+                    console.error('error occurred:', error)
+                    reject(error)
+                },
+                complete: () => {
+                    console.log('completed')
+                }
+            })
+        })
+    }
 }
